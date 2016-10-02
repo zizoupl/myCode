e6611dcefdc84547d2e1e93b79ce20e3c5bdf967
Krzysztof Orzel <krzysiek.orzel@gmail.com>
2016-10-02 18:59:15 +0200

    metody rozszerzeń

---
+++ myProject/myProject/Coding/ExtensionMethod/StringExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace myProject.Coding.ExtensionMethod
+{
+    /// <summary>
+    /// Extension metody zostały stworzone, aby dodawać metody do interfejsów,
+    /// oraz sealdowych metod! Są pisanie na publicznych rzeczach w obiekcie, chyba
+    /// że użyjemy refleksji 
+    /// </summary>
+    public static class StringExtension
+    {
+        public static bool IsPalindrome(this string obj)
+        {
+            return true;
+        }
+
+        public static bool IsPalindrome(this string obj, string napis)
+        {
+            return true;
+        }
+
+        public static bool Whatever<T>(this IEnumerable<T> obj)
+        {
+            return true;
+        }
+
+        //metoda na typ generyczny (dostępna zatem w każdym typie)
+        public static T IamExtensive<T>(this T obj) where T:IEnumerable //constraint
+        {
+            return default(T);
+        }
+
+
+        
+    }
+}
---
+++ myProject/myProject/Testing/ExtensionMethod/StringExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using myProject.Coding.ExtensionMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myProject.Testing.ExtensionMethod
+{   
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            "".IsPalindrome("");
+            "".IamExtensive();//generyczna
+            
+        }
+
+        [TestMethod]
+        public void TableTest()
+        {
+            string[] test = new[] {""};
+            test.Whatever();
+            test.IamExtensive();//generyczna
+        }
+    }
+}
